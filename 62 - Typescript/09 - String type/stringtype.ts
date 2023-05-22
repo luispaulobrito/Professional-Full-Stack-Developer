8b8495d8d0746a62b9e9c0023796a3df94cb1b1a
Luis Paulo <luiss_paullo@hotmail.com>
2023-05-22 15:31:43 -0300

    docker compose adicionado

---
+++ 62 - Typescript/09 - String type/stringtype.ts
@@ -0,0 +1,7 @@
+function sayHello(name:string) {
+    console.log(`Hello ${name}`);
+}
+
+let myName: string = 'Antonio'
+
+sayHello(myName)
---
+++ 62 - Typescript/10 - Number type/numbertype.ts
@@ -0,0 +1,5 @@
+let a : number = 9;
+let b : number = 22;
+
+let c = a+b;
+console.log(c)
