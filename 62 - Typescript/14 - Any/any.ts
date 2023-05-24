509e3bdca2cbf50c54993b5917885fd92f49c969
Luis Paulo <luiss_paullo@hotmail.com>
2023-05-23 22:02:32 -0300

    any e union types

---
+++ 62 - Typescript/14 - Any/any.ts
@@ -0,0 +1,9 @@
+let stuff: any
+
+stuff = 'Luis'
+stuff = 888
+stuff = false
+stuff = {name: 'Luis', hobbies: ['reading','play soccer']}
+stuff = [2, 5, 9]
+
+console.log(stuff);
---
+++ 62 - Typescript/15 - Union/union-type.ts
@@ -0,0 +1,13 @@
+let userId: number | string;
+
+userId = 'Luis';
+userId = 95;
+
+console.log(userId);
+
+let customer: (number | string)[] = [];
+customer.push(56)
+customer.push("José")
+console.log(customer);
+
+
